7349b426cf468cb76b692c133f6dc8408edb1262
Lenka Polcerova <lenka.polcerova@gmail.com>
2023-01-19 21:03:39 +0100

    splneni ukolu

---
+++ main.js
@@ -0,0 +1,17 @@
+// tady je místo pro náš program
+
+let nadpis
+nadpis = document.querySelector("h1");
+nadpis.style.color = "red";
+
+let odstavec
+odstavec = document.querySelector("p");
+odstavec.style.fontSize = "40px";
+
+let obrazek
+obrazek = document.querySelector("img");
+obrazek.src = "pes.jpg";
+
+let ctverec
+ctverec = document.querySelector(".ctverecek");
+ctverec.style.marginLeft = "50px";
